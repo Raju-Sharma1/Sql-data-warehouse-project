ee379d6434c988ea3d948dcc6e720c0bd35c6998
Raju Sharma <95718832+Raju-Sharma1@users.noreply.github.com>
2025-10-09 21:15:10 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,21 @@
+/*
+Creating Database and Schemas
+===============================
+
+Script Purpose:
+	This script creates a new Database called 'Datawarehouse' after checking If a database named Datawarehouse exists, kick everyone out of it,
+	roll back their transactions, and then delete that database completely. Subsequently, setting up 3 Schemas in the database: bronze, silver and gold.
+
+WARNING:
+	 Running this script will permanently DROP the database 'Datawarehouse'
+	 if it exists. ALL data inside the database will be LOST.
+	
+	 Proceed with EXTREME caution.
+	
+	 ✅ Make sure you have a recent BACKUP before executing this script.
+*/
+
+
 -- First Moving to Master Database to Create a Database in the system
 
 Use master;
