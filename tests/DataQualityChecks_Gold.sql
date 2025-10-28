1477c068b6d15eba315e7b71f18190da5c83a7ba
Raju Sharma <95718832+Raju-Sharma1@users.noreply.github.com>
2025-10-28 15:28:37 +0530

    Add quality checks for gold layer data integrity
    This script performs quality checks on the gold layer to validate data integrity, consistency, and accuracy. It includes checks for uniqueness of surrogate keys and referential integrity between fact and dimension tables.

---
+++ tests/DataQualityChecks_Gold.sql
@@ -0,0 +1,66 @@
+/*
+================
+Quality Checks
+================
+
+Purpose of the scripts:
+-----------------------
+      This script performs quality checks to validate the integrity, cosistency and accuracy of the gold layer.
+      
+      These Checks ensures:
+      --------------------
+          1. Uniqueness of surrogate keys in the dimension tables.
+          2. Refrential integrity between fact and dimension tables.
+          3. validation of relationships in the data model for analytical purposes.
+
+Usages Notes:
+------------
+  Run these checks after loading data to the Gold Layer.
+  Investigate and resolve any discrepancies found during the checks.
+=====================================================================
+*/
+
+
+/*
+============================
+gold.dim_customers - Checks
+============================
+*/
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+/*
+===========================
+gold.dim_products - Checks
+===========================
+*/
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+/*
+===========================
+gold.fact_sales - Checks
+===========================
+*/
+
+-- Check the data model connectivity between fact and dimensions
+
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL;
+
